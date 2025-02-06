397a8b916d097d8ae181121efbb2f77ebabe10ad
Даниэл Назарян <ndk985@yandex.ru>
2025-02-06 16:17:45 +0300

    anfisa2sprint_upgrade_v.3

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
+from django.db.models import Q
+
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'price', 'description'
+    ).filter(
+        is_published=True,
+        is_on_main=True,
+        category__is_published=True
+    )
+    # template = 'homepage/index.html'
+    # ice_cream_list = IceCream.objects.values(
+    #     'id', 'title', 'description', 'wrapper__title'
+    # ).filter(is_on_main=True, is_published=True).order_by('title')
+
+    #                   IceCream.objects.values(
+    #     'id', 'title', 'description'
+    # ).filter(
+    #     # Q(is_on_main=True)
+    #     # & Q(is_published=True)
+    #     # | Q(title__contains='пломбир')
+    #     # & Q(is_published=True)
+    #     is_published=True, is_on_main=True
+    # ).order_by('title')[:3]
+
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -6,7 +6,10 @@ from core.models import PublishedModel
 class Category(PublishedModel):
     title = models.CharField(max_length=256, verbose_name='Название')
     slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
-    output_order = models.PositiveSmallIntegerField(default=100, verbose_name='Порядок отображения')
+    output_order = models.PositiveSmallIntegerField(
+        default=100, 
+        verbose_name='Порядок отображения'
+    )
 
     class Meta:
         verbose_name = 'категория'
@@ -62,10 +65,16 @@ class IceCream(PublishedModel):
     )
     toppings = models.ManyToManyField(Topping, verbose_name='Топпинги')
     is_on_main = models.BooleanField(default=False, verbose_name='На главную')
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     class Meta:
         verbose_name = 'мороженое'
         verbose_name_plural = 'Мороженое'
-
+        ordering = ('output_order', 'title')
+        
     def __str__(self):
         return self.title
