b2c0da2b68b7d9250bfa8530bf0e570631a99406
Даниэл Назарян <ndk985@yandex.ru>
2025-02-23 18:55:51 +0300

    anfisa2sprint_upgrade_v.5

--- anfisa_for_friends/about/urls.py
+++ anfisa_for_friends/about/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'about'
 
 urlpatterns = [
-    path('', views.description, name='description'),
+    # path('', views.description, name='description'),
+    path('', views.Description.as_view(), name='description'),
 ]
--- anfisa_for_friends/about/views.py
+++ anfisa_for_friends/about/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+
+from contest.models import Contest
+
 
 def description(request):
     template = 'about/description.html'
     return render(request, template)
+
+
+class Description(TemplateView):
+    # В атрибуте template_name обязательно указывается имя шаблона,
+    # на основе которого будет создана возвращаемая страница.
+    template_name = 'about/description.html'
+    
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста из родительского метода.
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь ключ total_count;
+        # значение ключа — число объектов модели Birthday.
+        context['total_count'] = Contest.objects.count()
+        # Возвращаем изменённый словарь контекста.
+        return context
